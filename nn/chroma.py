adeab8428f75e1f9d14eda588a1d9404436505bc
Mathieu Croquelois <croquelois@gmail.com>
2025-06-11 21:07:53 +0100

    remove ada modulation

--- nn/chroma.py
+++ nn/chroma.py
@@ -125,7 +125,6 @@ class LastLayer(nn.Module):
         super().__init__()
         self.norm_final = nn.LayerNorm(hidden_size, elementwise_affine=False, eps=1e-6)
         self.linear = nn.Linear(hidden_size, patch_size * patch_size * out_channels, bias=True)
-        self.adaLN_modulation = nn.Sequential(nn.SiLU(), nn.Linear(hidden_size, 2 * hidden_size, bias=True))
 
     def forward(self, x, mod):
         shift, scale = mod
